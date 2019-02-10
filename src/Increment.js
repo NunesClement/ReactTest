8a36e69b6119cd11537420b93298e892cfbea0ae
Clément Nunes <themega19@gmail.com>
2019-02-10 23:50:40 +0100

    test

--- src/Increment.js
+++ src/Increment.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 
 function Increment() {
   const [count, setCount] = useState(0);
+  const [mail, setMail] = useState("test@test.fr");
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
   });
-
   return (
     <div className="col-md-12 text-center">
       <p>Vous avez cliqué {count} fois</p>
@@ -16,6 +16,10 @@ function Increment() {
       <button className="btn btn-primary" onClick={() => setCount(count - 1)}>
         -
       </button>
+      <br />
+      <br />
+      <input className="form-control" onClick={() => setMail(mail + "\n")} />
+      <p>{mail}</p>
     </div>
   );
 }
